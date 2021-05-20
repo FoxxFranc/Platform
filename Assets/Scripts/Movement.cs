240b833c87185cfb8eef8e0ac15bfe7ea94884ac
FoxxFranc <74159513+FoxxFranc@users.noreply.github.com>
2021-05-20 18:51:59 +0300

    Init

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Transform))]
+
+public class Movement : MonoBehaviour
+{
+    private Transform _transform;
+    
+    [SerializeField] private float _speed;
+    [SerializeField] private float _jumpForce;
+
+    private void Start()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    void Update()
+    {      
+        transform.Translate(new Vector2(Input.GetAxis("Horizontal") * _speed * Time.deltaTime, 0));
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(new Vector2(0, _jumpForce * Time.deltaTime));
+        }
+    }
+}
